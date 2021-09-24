960d11e12905e60770962238dad53451ddad01ac
Bozhidar-Vangelov <bozhidar.vangelov@gmail.com>
2021-09-24 19:01:00 +0300

    Add image logic added

--- handlers/cat.js
+++ handlers/cat.js
@@ -2,9 +2,10 @@ const url = require('url');
 const fs = require('fs');
 const path = require('path');
 const qs = require('querystring');
-// const formidable = require('formidable');
+const formidable = require('formidable');
 const breeds = require('../data/breeds.json');
 const cats = require('../data/cats.json');
+const globalPath = '/home/bvangelov/projects/cat-shelter';
 
 module.exports = (req, res) => {
   const pathname = url.parse(req.url).pathname;
@@ -75,6 +76,53 @@ module.exports = (req, res) => {
       res.writeHead(302, { location: '/' });
       res.end();
     });
+  } else if (pathname === '/cats/add-cat' && req.method === 'POST') {
+    let form = new formidable.IncomingForm();
+
+    form.parse(req, (err, fields, files) => {
+      if (err) {
+        throw err;
+      }
+
+      let oldPath = files.upload.path;
+      let newPath = path.normalize(
+        path.join(globalPath, `/content/images/${files.upload.name}`)
+      );
+      console.log(oldPath);
+      console.log(newPath);
+
+      fs.rename(oldPath, newPath, (err) => {
+        if (err) {
+          throw err;
+        }
+        console.log('File was uploaded successfully');
+      });
+    });
+
+    // req.on('data', (data) => {
+    //   formData += data;
+    // });
+
+    // req.on('end', () => {
+    //   let body = qs.parse(formData);
+
+    //   fs.readFile('./data/breeds.json', (err, data) => {
+    //     if (err) {
+    //       throw err;
+    //     }
+
+    //     let breeds = JSON.parse(data);
+    //     breeds.push(body.breed);
+    //     let json = JSON.stringify(breeds);
+
+    //     fs.writeFile('./data/breeds.json', json, 'utf-8', () =>
+    //       console.log('The breed was uploaded successfully!')
+    //     );
+    //   });
+
+    //   res.writeHead(302, { location: '/' });
+    //   res.end();
+    // });
   } else {
     return true;
   }
